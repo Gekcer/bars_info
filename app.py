4d4dc5bed6707422ed8b29e45f57121f2322e612
Gekcer <gekcer@gmail.com>
2024-07-03 16:54:15 +0300

    подготовил app.py для docker-compose

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
+from dotenv import load_dotenv
 
 from app import db
 from app.resources import BarResource
 from app.resources import BarModel
 
+load_dotenv()
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/krasnodarbars'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
@@ -15,4 +20,4 @@ db.init_app(app)
 api.add_resource(BarResource, '/bar/num_<int:id>', '/bar')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
