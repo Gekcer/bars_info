e5ed1fa42310b9e445f9a06a127f5bc039473d60
Gekcer <gekcer@gmail.com>
2024-07-03 16:55:10 +0300

    обновил заполнение баров

--- db/fill_bars.py
+++ db/fill_bars.py
@@ -1,12 +1,19 @@
+import os
+
 from sqlalchemy import create_engine, MetaData, Column, String, Integer
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
 import json
+
+load_dotenv()
+
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 with open('bars.json', 'r') as j:
     bars = json.load(j)
 
-engine = create_engine('postgresql://postgres:postgres@localhost:5432/krasnodarbars')
+engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
 session = Session()
 
